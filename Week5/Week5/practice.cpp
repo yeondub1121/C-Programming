f767b0e787b5353b320d0beabc1197f7fb47352d
yeondub1121 <kssjshyjj@naver.com>
2025-10-10 13:55:24 +0900

    Feat:Week 5 Practice

--- Week5/Week5/practice.cpp
+++ Week5/Week5/practice.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
-#include <string>
 using namespace std;
+
+class Circle {
+    int radius;
+
+public:
+    Circle() { radius = 1; }
+    Circle(int radius) { this->radius = radius; }
+
+    void setRadius(int radius) {
+        this->radius = radius;
+    }
+
+    double getArea() {
+        return 3.14 * radius * radius;
+    }
+};
+
 int main() {
-	// 스트링생성
-	string str; // 빈문자열을가진스트링객체생성
-	string address("서울시성북구삼선동389");
-	string copyAddress(address); // address의문자열을복사한스트링객체생성
-	char text[] = { 'L', 'o', 'v', 'e', ' ', 'C', '+', '+', '\0' }; // C-스트링
-	string title(text); // "Love C++" 문자열을가진스트링객체생성
-	// 스트링출력
-	cout << str << endl; // 빈스트링. 아무값도출력되지않음
-	cout << address << endl;
-	cout << copyAddress << endl;
-	cout << title << endl;
+    Circle circle;
+    Circle& refc = circle;
+
+    refc.setRadius(10);
+    cout << refc.getArea() << " " << circle.getArea();
 }
