1af114dbebd1f9ea58eafa3bcac44baf7f55e2cc
yeondub1121 <kssjshyjj@naver.com>
2025-11-20 15:22:15 +0900

    Week10 done

---
+++ Week10/Week10/Practice.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Power {
+    int kick;
+    int punch;
+public:
+    Power(int kick = 0, int punch = 0) {
+        this->kick = kick; this->punch = punch;
+    }
+    void show();
+    Power operator++ (int x); 
+};
+
+void Power::show() {
+    cout << "kick=" << kick << ',' << "punch=" << punch << endl;
+}
+
+Power Power::operator++(int x) {
+    Power tmp = *this;   
+    kick++;
+    punch++;
+    return tmp;         
+}
+
+int main() {
+    Power a(3, 5), b;
+    a.show();
+    b.show();
+    b = a++;  
+    a.show(); /
+    b.show(); 
+}
