96c2ac32c9e641272a250f035d023578ecf9382a
yeondub1121 <kssjshyjj@naver.com>
2025-12-18 23:56:22 +0900

    Feat: final

---
+++ 14Weekl/14Weekl/Practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int big(int a, int b, int c = 100) {
+    int m = (a > b) ? a : b;
+    return (m > c) ? c : m;
+}
+
+int main() {
+    int x = big(3, 5);       // 3과 5중 큰 값 5는 최대값 100보다 작으므로, 5 리턴
+    int y = big(300, 60);    // 300과 60중 큰 값 300이 최대값 100보다 크므로, 100 리턴
+    int z = big(30, 60, 50); // 30과 60 중 큰 값 60이 최대값 50보다 크므로, 50 리턴
+    cout << x << ' ' << y << ' ' << z << endl;
+}
--- Week12/Week12/Practice.cpp
+++ Week12/Week12/Practice.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
 
-    vector<int>::iterator it;
-
-    for (it = v.begin(); it != v.end(); it++) {
-        int n = *it;
-        n = n * 2;
-        *it = n;
+    cout << "5개의 정수를 입력하세요 >> ";
+    for (int i = 0; i < 5; i++) {
+        int n;
+        cin >> n;
+        v.push_back(n);
     }
 
+    sort(v.begin(), v.end());
+
+    vector<int>::iterator it;
     for (it = v.begin(); it != v.end(); it++)
         cout << *it << ' ';
 
